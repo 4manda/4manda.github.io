36c033b57d246f36b2542ea289222cfb06c6be55
Amanda Flink <amanda.j.flink@gmail.com>
2019-06-11 12:29:46 -0700

    [clean] Remove unused files and rebuild

--- src/middleware/loggingMiddleware.js
+++
@@ -1,8 +0,0 @@
-const loggingMiddleware = (store) => (next) => (action) => {
-  console.log('Redux Log:', action);
-  // call the next function
-  let result = next(action);
-  return result;
-}
-
-export default loggingMiddleware;
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-export const initialState = {};
-
-export function rootReducer(state = initialState, action) {
-  return state;
-};
-
